4e1ad96ca36b80e18e17a730104cda03e817af08
DenisiaBianca <63000994+DenisiaBianca@users.noreply.github.com>
2020-10-25 13:17:18 +0200

    Add files via upload

---
+++ ParaialaDenisia/L02/StudentsApp/StudentsApp/StudentsRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsApp
+{
+    public static class StudentsRepo
+    {
+        public static List<Student> StudentsList = new List<Student>()
+        {
+            new Student
+            {
+                Id=1,
+                Name="Popescu Andrei",
+                Faculty="AC",
+                Specialization="IS",
+                YearOfStudy=3
+            },
+            new Student
+            {
+                Id=2,
+                Name="Marinescu Ana",
+                Faculty="AC",
+                Specialization="CTI",
+                YearOfStudy=2
+            }
+        };
+    }
+}
