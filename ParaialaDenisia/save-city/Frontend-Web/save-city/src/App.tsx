f411cc224b1bf95a4f2a350622ff8c5f035b33ca
DenisiaBianca <63000994+DenisiaBianca@users.noreply.github.com>
2020-12-16 19:21:00 +0200

    add react app

---
+++ ParaialaDenisia/save-city/Frontend-Web/save-city/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LoginPage from "./Pages/LoginPage";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage></LoginPage>
+    </div>
+  );
+}
+
+export default App;
